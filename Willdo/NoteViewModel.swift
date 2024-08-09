4f2e2315a823b7d91db677409b9c8645c474c7eb
veto <veto@myridia.com>
2024-08-09 10:38:08 +0700

    add more base sqlite queries handlers

--- Willdo/NoteViewModel.swift
+++ Willdo/NoteViewModel.swift
@@ -12,16 +12,17 @@ import SQLite3
 class NoteViewModel: ObservableObject {
     @Published var notes: [Note] = []
     var db : OpaquePointer?
-    var path : String = "willdo.sqlite"
+    var path : String = "willdo8.sqlite"
     init(){
         self.db = createDB()
         self.createTable()
-        self.insertData(_id:"aa",_rev:"rrr",doc:"dddd")
+        self.insertData(_id:"idididididid",_rev:"revrev",doc:"dockdock")
+        self.read()
         
     }
     
     func insertData(_id:String,_rev:String,doc:String) {
-        let query = "INSERT INTO willdo (_id, _rev) VALUES (?,?);"
+        let query = "INSERT INTO willdo (_id, _rev, doc) VALUES (?,?,?);"
         
         var statement : OpaquePointer? = nil
         
@@ -58,8 +59,62 @@ class NoteViewModel: ObservableObject {
             }
         }
     
+    
+    func read() {
+        //var notes: [Note] = []
+
+        
+           // var mainList = [DBGrade]()
+            
+            let query = "SELECT _id, _rev, doc FROM willdo;"
+            var statement : OpaquePointer? = nil
+            if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK{
+                while sqlite3_step(statement) == SQLITE_ROW {
+                    
+                    let _id = String(describing: String(cString: sqlite3_column_text(statement, 0)))
+                    let _rev = String(describing: String(cString: sqlite3_column_text(statement, 1)))
+                    let doc = String(describing: String(cString: sqlite3_column_text(statement, 2)))
+                    print(_id)
+        
+              
+                }
+            }
+            //return mainList
+
+         
+         }
+    /*
+     func update(id : Int, name : String, result : String, avg : Int, list : [Grade]) {
+           let data = try! JSONEncoder().encode(list)
+           let listString = String(data: data, encoding: .utf8)
+           let query = "UPDATE grade SET name = '\(name)', result = '\(result)', avg = \(avg), list = '\(listString!)' WHERE id = \(id);"
+           var statement : OpaquePointer? = nil
+           if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK{
+               if sqlite3_step(statement) == SQLITE_DONE {
+                   print("Data updated success")
+               }else {
+                   print("Data is not updated in table")
+               }
+           }
+       }
+       
+       func delete(id : Int) {
+           let query = "DELETE FROM grade where id = \(id)"
+           var statement : OpaquePointer? = nil
+           if sqlite3_prepare_v2(db, query, -1, &statement, nil) == SQLITE_OK{
+               if sqlite3_step(statement) == SQLITE_DONE {
+                   print("Data delete success")
+               }else {
+                   print("Data is not deleted in table")
+               }
+           }
+       }
+     
+     
+    */
+    
     func createTable()  {
-            let query = "CREATE TABLE IF NOT EXISTS willdo(id INTEGER PRIMARY KEY AUTOINCREMENT,_id TEXT, _rev TEXT, doc TEXT);"
+            let query = "CREATE TABLE IF NOT EXISTS willdo(id INTEGER PRIMARY KEY AUTOINCREMENT, _id TEXT, _rev TEXT, doc TEXT);"
             var statement : OpaquePointer? = nil
             
             if sqlite3_prepare_v2(self.db, query, -1, &statement, nil) == SQLITE_OK {
@@ -93,7 +148,7 @@ class NoteViewModel: ObservableObject {
         if content != "" {
             var title = title
             if title == "" {
-                //https://www.zerotoappstore.com/how-to-split-a-string-in-swift-with-examples.html
+                //  https://www.zerotoappstore.com/how-to-split-a-string-in-swift-with-examples.html
                 let a = content.components(separatedBy: "\n")
                 if a[0].count > 25 {
                     //let slice = content[0..<50]
@@ -112,6 +167,8 @@ class NoteViewModel: ObservableObject {
             }
             notes.append(Note(title: title, content:content))
             print("...add note")
+            self.insertData(_id:title,_rev:"rrr",doc:content)
+
         }
         
     }
